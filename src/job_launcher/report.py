790550e12296d0ac0c60a5162a49a053bc90fdf4
kamelov <kamelovnn@gmail.com>
2023-12-19 18:32:48 +0500

    merged changes

--- src/job_launcher/report.py
+++ src/job_launcher/report.py
@@ -1,5 +1,7 @@
 import json
 import logging
+
+from jinja2 import Template
 from os import path
 from typing import Dict
 
@@ -15,21 +17,29 @@ def dump_json_report(data: Dict, output: str):
 
 
 class Reporter:
-    HTML_REPORT = 'job_launcher_result.html'
+    HTML_REPORT = "job_launcher_result.html"
 
     def __init__(self, output: str):
         self.json_report_file = path.join(output, JSON_REPORT)
         self.html_report_file = path.join(output, self.HTML_REPORT)
 
     def generate(self):
-        log.info('Start report generation')
+        log.info("Start report generation")
+
         json_report = self._load_json_report()
-        message = self._get_message(
-            json_report.get('server'),
-            json_report.get('results')
-        )
+        message = self._get_message(json_report.get("server"), json_report.get("results"))
         log.info(message)
-        log.info('Finish report generation')
+        log.info("Json report generated")
+
+        html_template = Template("resources/templates/report.html.j2")
+        html_report = html_template.render(
+            server=json_report.get('server'),
+            results=json_report.get('results')
+        )
+        log.info(html_report)
+        log.info('HTML report generated')
+
+        log.info("Finish report generation")
 
     def _load_json_report(self):
         with open(self.json_report_file) as f:
@@ -37,12 +47,11 @@ class Reporter:
 
     def _get_message(self, server, results=None):
         results = results or []
-        message = [f'Jenkins server: {server}']
+        message = [f"Jenkins server: {server}"]
         for result in results:
             message.append(f'name: {result.get("name")}')
             message.append(f'status: {result.get("status")}')
             message.append(f'timestamp: {result.get("result", {}).get("timestamp") or "-"}')
             message.append(f'number: {result.get("result", {}).get("number") or "-"}')
-            message.append('')
-        return '\n'.join(message)
-
+            message.append("")
+        return "\n".join(message)
